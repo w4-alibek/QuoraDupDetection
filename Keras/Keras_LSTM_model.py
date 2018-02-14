c39a0f4e4b89f97379be91bea65310d8ff13ac4e
w4-alibek <alibek@wisefour.com>
2018-02-14 10:04:04 +0900

    Add Flags for controlling features and settings.

--- Keras/Keras_LSTM_model.py
+++ Keras/Keras_LSTM_model.py
@@ -1,5 +1,5 @@
 """Trains LSTM model with train.csv. How to run:
-python Keras_LSTM_model.py --raw_train_data ~chungshik/quora_data/data/train.csv --glove_path ~chungshik/quora_data/word_embeddings/glove.840B.300d.txt --glove_dimension 300 --batch_size 100
+python Keras_LSTM_model.py --raw_train_data ~chungshik/quora_data/data/train.csv --word_embedding_path ~chungshik/quora_data/word_embeddings/glove.840B.300d.txt --embedding_vector_dimension 300 --batch_size 100
 """
 import tensorflow as tf
 import glove_embedding as embedding
@@ -15,39 +15,68 @@ from keras.preprocessing.sequence import pad_sequences
 from keras import optimizers
 import numpy
 
-tf.flags.DEFINE_integer("lstm_out_dimantion", 100,
-                       "Where the train data with computed features are stored.")
 tf.flags.DEFINE_float("recurrent_dropout", 0.2,
+                       "Where the word_to_id is stored.")
+tf.flags.DEFINE_integer("max_sequence_length", 1000,
+                       "Maximum length of question length")
+
+# Word embeddings
+tf.flags.DEFINE_string("word_embedding_type", "glove",
+                       "Word embedding vectors. One of 'glove', 'word2vec'.")
+tf.flags.DEFINE_string("embedding_by_word_id_path", None,
+                       "Where the word ID to embedding vector is stored.")
+tf.flags.DEFINE_string("word_embedding_path", '',
+                       "Where the word embedding vectors are located.")
+tf.flags.DEFINE_integer("embedding_vector_dimension", None,
+                        "Word embedding vector's dimension.")
+
+
+# Training
+tf.flags.DEFINE_bool("remove_stopwords", True, "Remove stop words")
+tf.flags.DEFINE_integer("batch_size", 100, "Batch size")
+tf.flags.DEFINE_float("learning_rate", 0.002, "Learning rate")
+tf.flags.DEFINE_string("optimizer", "adam",
+                       "Optimization method. One of 'adadelta', 'adam', "
+                       "'sgd', 'adagrad', 'rmsprop'")
+tf.flags.DEFINE_integer("num_epochs", 20, "Number of epochs")
+tf.flags.DEFINE_integer("log_epoch", 100, "Period at which to log results")
+tf.flags.DEFINE_string("loss_function",
+                       "l2_loss", "Loss function can be one of 'l2_loss' or 'contrastive_loss'")
+
+# np.set_printoptions(threshold=np.nan)
+
+tf.flags.DEFINE_string("processed_train_data", None,
+                       "Where the train data with computed features are stored.")
+tf.flags.DEFINE_string("word_to_id_path", None,
                        "Where the word_to_id is stored.")
 tf.flags.DEFINE_string("raw_train_data", None,
                        "Where the raw train data is stored.")
-tf.flags.DEFINE_string("glove_path", None,
-                       "Where the glove embedding data is stored.")
 tf.flags.DEFINE_string("raw_test_data", None,
-                       "Where the raw test data is stored.")
-tf.flags.DEFINE_integer("batch_size", 100,
-                       "Batch size.")
-tf.flags.DEFINE_integer("max_sequence_length", 1000,
-                       "Maximum length of question length")
-tf.flags.DEFINE_integer("number_epochs", 15,
-                       "Number of epochs to train")
-tf.flags.DEFINE_integer("glove_dimension", 50,
-                       "Glove embedding dimension")
-tf.flags.DEFINE_string("model", "lstm",
-                       "Which model to use for training")
+                       "Where the raw train data is stored.")
+
+# LSTM model
+tf.flags.DEFINE_integer("lstm_out_dimension", 50,
+                        "Hidden state dimension (LSTM output vector dimension)")
+tf.flags.DEFINE_integer("num_steps", 5,
+                        "Number of time steps for LSTM")
+
+# Model selection.
+tf.flags.DEFINE_string(
+    "model", "base_model",
+    "Name of a model to run. One of 'base_model', 'bidirectional_rnn', 'qrnn'.")
 
 FLAGS = tf.flags.FLAGS
 VALIDATION_SPLIT = 0.2
 
 embedding_layer, labels, question1s, question2s, tokenizer = embedding\
-    .process_data(FLAGS.glove_path,
+    .process_data(FLAGS.word_embedding_path,
                   FLAGS.raw_train_data,
-                  FLAGS.glove_dimension,
+                  FLAGS.embedding_vector_dimension,
                   FLAGS.max_sequence_length)
 
-lstm_layer = LSTM(FLAGS.lstm_out_dimantion, recurrent_dropout=FLAGS.recurrent_dropout)
+lstm_layer = LSTM(FLAGS.lstm_out_dimension, recurrent_dropout=FLAGS.recurrent_dropout)
 
-if FLAGS.model == "bidirectional_lstm":
+if FLAGS.model == "bidirectional_rnn":
     lstm_layer = Bidirectional(lstm_layer, merge_mode='concat')
 
 sequence_1_input = Input(shape=(FLAGS.max_sequence_length,), dtype="int32")
@@ -57,7 +86,6 @@ x1 = lstm_layer(embedded_sequences_1)
 sequence_2_input = Input(shape=(FLAGS.max_sequence_length,), dtype="int32")
 embedded_sequences_2 = embedding_layer(sequence_2_input)
 y1 = lstm_layer(embedded_sequences_2)
-
 
 
 sequences1 = tokenizer.texts_to_sequences(question1s)
@@ -114,7 +142,7 @@ model_checkpoint = ModelCheckpoint(best_model_path,
 history = model.fit([q1_train, q2_train],
                 labels_train,
                 validation_data=([q1_validation, q2_validation], labels_validation),
-                epochs=FLAGS.number_epochs,
+                epochs=FLAGS.num_epochs,
                 batch_size=FLAGS.batch_size,
                 shuffle=True,
                 callbacks=[early_stopping, model_checkpoint, logging, csv_logger],
