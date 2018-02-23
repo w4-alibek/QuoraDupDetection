ca7a3fe9f7ab6eed08baf89eae493e08b440a0d1
w4-alibek <alibek@wisefour.com>
2018-02-23 19:36:58 +0900

    Feature engineering.

---
+++ Keras/features.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Utility functions
+refs: https://github.com/aerdem4/kaggle-quora-dup
+"""
+from fuzzywuzzy import fuzz
+from nltk.corpus import stopwords
+import pandas as pd
+import re
+import tensorflow as tf
+import util
+
+STOP_WORDS = stopwords.words("english")
+
+tf.flags.DEFINE_string("raw_train_data", None, "Where the raw train data is stored.")
+tf.flags.DEFINE_string("raw_test_data", None, "Where the raw train data is stored.")
+
+FLAGS = tf.flags.FLAGS
+
+
+def word_net_lemmatize(text):
+    """Normalize the given list of words. Return list of normalized word
+    """
+    return ' '.join(
+        [WordNetLemmatizer.lemmatize(WordNetLemmatizer.lemmatize(word, "n"), "v")
+         for word in text.split()])
+
+
+def clean_text(text):
+    text = text\
+        .lower()\
+        .replace(",000,000", "m")\
+        .replace(",000", "k")\
+        .replace("′", "'")\
+        .replace("’", "'")\
+        .replace("won't", "will not")\
+        .replace("cannot", "can not")\
+        .replace("can't", "can not")\
+        .replace("n't", " not")\
+        .replace("what's", "what is")\
+        .replace("it's", "it is")\
+        .replace("'ve", " have")\
+        .replace("i'm", "i am")\
+        .replace("'re", " are")\
+        .replace("he's", "he is")\
+        .replace("she's", "she is")\
+        .replace("'s", " own")\
+        .replace("%", " percent ")\
+        .replace("₹", " rupee ")\
+        .replace("$", " dollar ")\
+        .replace("€", " euro ")\
+        .replace("'ll", " will")\
+        .replace("=", " equal ")\
+        .replace("+", " plus ")
+
+    text = re.sub('[“”\(\'…\)\!\^\"\.;:,\-\?？\{\}\[\]\\/\*@]', ' ', text)
+    text = re.sub(r"([0-9]+)000000", r"\1m", text)
+    text = re.sub(r"([0-9]+)000", r"\1k", text)
+    return text
+
+
+def remove_stop_words_and_punctuation(text):
+    """Given text as string. Removes stop words from text and
+    return list of words without stop word
+    """
+    stop_words = stopwords.words('english')
+    stop_words.extend(list(string.punctuation))
+    stop_words = set(stop_words)
+    word_tokens = word_tokenize(text)
+    filtered_words = [w for w in word_tokens if not w in stop_words]
+    return ' '.join(filtered_words)
+
+
+def get_token_features(question1, question2):
+    token_features = [0.0]*10
+
+    question1_tokens = question1.split()
+    question2_tokens = question2.split()
+
+    if len(question1_tokens) == 0 or len(question2_tokens) == 0:
+        return token_features
+
+    question1_words = set([word for word in question1_tokens if word not in STOP_WORDS])
+    question2_words = set([word for word in question2_tokens if word not in STOP_WORDS])
+
+    question1_stops = set([word for word in question1_tokens if word in STOP_WORDS])
+    question2_stops = set([word for word in question2_tokens if word in STOP_WORDS])
+
+    common_word_count = len(question1_words.intersection(question2_words))
+    common_stop_count = len(question1_stops.intersection(question2_stops))
+    common_token_count = len(set(question1_tokens).intersection(set(question2_tokens)))
+
+    token_features[0] = common_word_count / (min(len(question1_words),
+                                                 len(question2_words)) + 0.0001)
+    token_features[1] = common_word_count / (max(len(question1_words),
+                                                 len(question2_words)) + 0.0001)
+    token_features[2] = common_stop_count / (min(len(question1_stops),
+                                                 len(question2_stops)) + 0.0001)
+    token_features[3] = common_stop_count / (max(len(question1_stops),
+                                                 len(question2_stops)) + 0.0001)
+    token_features[4] = common_token_count / (min(len(question1_tokens),
+                                                  len(question2_tokens)) + 0.0001)
+    token_features[5] = common_token_count / (max(len(question1_tokens),
+                                                  len(question2_tokens)) + 0.0001)
+    token_features[6] = int(question1_tokens[-1] == question2_tokens[-1])
+    token_features[7] = int(question1_tokens[0] == question2_tokens[0])
+    token_features[8] = abs(len(question1_tokens) - len(question2_tokens))
+    token_features[9] = (len(question1_tokens) + len(question2_tokens))/2
+    return token_features
+
+
+
+def get_longest_substr_ratio(question1, question2):
+    strs = list(util.lcsubstrings(question1, question2))
+    if len(strs) == 0:
+        return 0
+    else:
+        return len(strs[0]) / (min(len(question1), len(question2)) + 1)
+
+
+def extract_features(nlp_features):
+    nlp_features["question1"] = nlp_features["question1"].fillna("").apply(clean_text)
+    nlp_features["question2"] = nlp_features["question2"].fillna("").apply(clean_text)
+
+    print("token features...")
+    token_features = nlp_features.apply(
+        lambda x: get_token_features(x["question1"], x["question2"]), axis=1)
+    nlp_features["cwc_min"] = list(map(lambda x: x[0], token_features))
+    nlp_features["cwc_max"] = list(map(lambda x: x[1], token_features))
+    nlp_features["csc_min"] = list(map(lambda x: x[2], token_features))
+    nlp_features["csc_max"] = list(map(lambda x: x[3], token_features))
+    nlp_features["ctc_min"] = list(map(lambda x: x[4], token_features))
+    nlp_features["ctc_max"] = list(map(lambda x: x[5], token_features))
+    nlp_features["last_word_eq"] = list(map(lambda x: x[6], token_features))
+    nlp_features["first_word_eq"] = list(map(lambda x: x[7], token_features))
+    nlp_features["abs_len_diff"] = list(map(lambda x: x[8], token_features))
+    nlp_features["mean_len"] = list(map(lambda x: x[9], token_features))
+
+    print("fuzzy features..")
+    nlp_features["token_set_ratio"] = nlp_features.apply(
+        lambda x: fuzz.token_set_ratio(x["question1"], x["question2"]), axis=1)
+    nlp_features["token_sort_ratio"] = nlp_features.apply(
+        lambda x: fuzz.token_sort_ratio(x["question1"], x["question2"]), axis=1)
+    nlp_features["fuzz_ratio"] = nlp_features.apply(
+        lambda x: fuzz.QRatio(x["question1"], x["question2"]), axis=1)
+    nlp_features["fuzz_partial_ratio"] = nlp_features.apply(
+        lambda x: fuzz.partial_ratio(x["question1"], x["question2"]), axis=1)
+    nlp_features["longest_substr_ratio"] = nlp_features.apply(
+        lambda x: get_longest_substr_ratio(x["question1"], x["question2"]), axis=1)
+    return nlp_features
+
+print("Extracting features for train...")
+train_nlp_features = pd.read_csv(FLAGS.raw_train_data)
+train_nlp_features = extract_features(train_nlp_features)
+train_nlp_features.drop(["id", "qid1", "qid2", "question1", "question2", "is_duplicate"],
+                        axis=1,
+                        inplace=True)
+train_nlp_features.to_csv("data/nlp_features_train.csv", index=False)
+
+print("Extracting features for test...")
+test_nlp_features = pd.read_csv(FLAGS.raw_test_data)
+test_nlp_features = extract_features(test_nlp_features)
+test_nlp_features.drop(["test_id", "question1", "question2"], axis=1, inplace=True)
+test_nlp_features.to_csv("data/nlp_features_test.csv", index=False)
--- Keras/glove_embedding.py
+++ Keras/glove_embedding.py
@@ -4,19 +4,20 @@ from keras.layers import Embedding
 from keras.preprocessing.text import Tokenizer
 import numpy as np
 import pandas as pd
+from nltk.stem import WordNetLemmatizer
 
 import read_data
-import util
+import features
 
 def process_data(glove_embedding_path, raw_train_data, embedding_dim, max_sequence_length):
     embeddings_index = read_data.read_GloVe(glove_embedding_path)
 
     train = pd.read_csv(raw_train_data)
 
-    train["question1"] = train["question1"].fillna("").apply(util.clean_text)\
-        .apply(util.remove_stop_words_and_punctuation)
-    train["question2"] = train["question2"].fillna("").apply(util.clean_text)\
-        .apply(util.remove_stop_words_and_punctuation)
+    train["question1"] = train["question1"].fillna("").apply(features.clean_text) \
+        .apply(features.remove_stop_words_and_punctuation).apply(features.word_net_lemmatize)
+    train["question2"] = train["question2"].fillna("").apply(features.clean_text)\
+        .apply(features.remove_stop_words_and_punctuation).apply(features.word_net_lemmatize)
 
     labels = np.array(train["is_duplicate"])  # list of label ids
     question1_list = np.array(train["question1"])
@@ -35,12 +36,21 @@ def process_data(glove_embedding_path, raw_train_data, embedding_dim, max_sequen
     num_words =len(word_index) + 1
     embedding_matrix = np.zeros((num_words, embedding_dim))
 
+    count_none = 0
+    back_of_words = []
     for word, i in word_index.items():
         embedding_vector = embeddings_index.get(word)
+        if embedding_vector is None:
+            count_none = count_none + 1
+            back_of_words.append(word)
+
         if embedding_vector is not None:
             # words not found in embedding index will be all-zeros.
             embedding_matrix[i] = embedding_vector
 
+    print('# not found words', count_none)
+    not_found_words = pd.DataFrame({"word": back_of_words})
+    not_found_words.to_csv("./with_norm.csv", index=False)
     # load pre-trained word embeddings into an Embedding layer
     # note that we set trainable = False so as to keep the embeddings fixed
     embedding_layer = Embedding(num_words,
