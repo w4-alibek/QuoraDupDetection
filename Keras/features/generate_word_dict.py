f51fa07d56d944be6bf5d18d152364dad8247497
w4-alibek <alibek@wisefour.com>
2018-03-04 17:29:26 +0900

    fix bug

--- Keras/features/generate_word_dict.py
+++ Keras/features/generate_word_dict.py
@@ -37,26 +37,34 @@ def create_word_dict():
     question1_list = np.array(train["question1"])
     question2_list = np.array(train["question2"])
 
-    # finally, vectorize the text samples into a 2D integer tensor
+    # Get word count
     tokenizer = Tokenizer(filters="")
     tokenizer.fit_on_texts(np.append(question1_list, question2_list))
     word_freq = tokenizer.word_counts
+
+    # Sort by word_count
     word_freq = list(sorted(word_freq.iteritems(),
                             key=lambda x: x[1],
                             reverse=True))
-    top_2000_words = word_freq[:2000]
+    # Get top 2000 words
+    word_freq = word_freq[:2000]
 
-    set_of_words = set(word for word, value in top_2000_words)
+    # Generate freq dict
+    freq_dict = {}
+    for word_count in word_freq:
+        freq_dict[word_count[0]] = word_count[1]
 
-    train["question1"] = train["question1"].apply(lambda x: remove_rare_words(x, set_of_words))
-    train["question2"] = train["question2"].apply(lambda x: remove_rare_words(x, set_of_words))
+    word_freq = [word for word, value in word_freq]
+
+    train["question1"] = train["question1"].apply(lambda x: remove_rare_words(x, word_freq))
+    train["question2"] = train["question2"].apply(lambda x: remove_rare_words(x, word_freq))
 
     print("Generating ID for words")
-    top_words = pd.DataFrame(top_2000_words)[0].drop_duplicates()
+    top_words = pd.DataFrame(word_freq)[0].drop_duplicates()
     top_words.reset_index(inplace=True, drop=True)
 
     print("Number of unique words:", len(top_words))
-    return top_words, train, set_of_words, word_freq
+    return top_words, train, word_freq, freq_dict
 
 
 def normalize_feature(feature_weight, max_weight):
@@ -64,8 +72,8 @@ def normalize_feature(feature_weight, max_weight):
 
 
 def build_graph():
-    max_weight = -109000000;
-    top_words, dataset, set_of_words, word_freq = create_word_dict()
+    max_weight = -100000000
+    top_words, dataset, set_of_words, freq_dict = create_word_dict()
     word_dict = pd.Series(top_words.index.values, index=top_words.values).to_dict()
     print (word_dict["best"])
     graph = networkx.MultiGraph()
@@ -95,20 +103,33 @@ def build_graph():
                 else:
                     graph.add_edge(node_a, node_b, weight=edge_weight)
 
-    print "maximum weight from edge building", max_weight
-    return graph, set_of_words, word_dict, dataset, word_freq
+    print "Maximum edge weight from graph build", max_weight
+    return graph, set_of_words, word_dict, dataset, freq_dict
 
-# top_7_words
+
+def top_7_words(word_freq, word_list):
+    sorted_by_freq = sorted([(word, word_freq[word]) for word in word_list],
+                            key=lambda tup: tup[1],
+                            reverse=True)
+    if len(sorted_by_freq) > 7:
+        sorted_by_freq = sorted_by_freq[:7]
+    return [word_count[0] for word_count in sorted_by_freq]
+
 
 def generate_feature(graph, word_dict, dataset, word_freq, category):
     # Now compute the actual feature after preprocess.
     feature = []
-    max_edge_weight = 1.0
+    max_edge_weight = -100000000
     for question1, question2 in np.stack((dataset["question1"], dataset["question2"]), axis=-1):
         feature_weight = 0.0
-        # word_list_1 = top_7_words(word_freq, question1.split())
-        for word_q1 in question1.split():
-            for word_q2 in question2.split():
+
+        # Get top 7 words from question1 and question2
+        top_words_1 = top_7_words(word_freq, question1.split())
+        top_words_2 = top_7_words(word_freq, question2.split())
+
+        # Generate feature for question comparison
+        for word_q1 in top_words_1:
+            for word_q2 in top_words_2:
                 node_a = word_dict[word_q1]
                 node_b = word_dict[word_q2]
 
@@ -120,16 +141,20 @@ def generate_feature(graph, word_dict, dataset, word_freq, category):
 
                 if graph.has_edge(node_a, node_b):
                     feature_weight = feature_weight + graph[node_a][node_b][0]['weight']
-                    sign = -1 if feature_weight < 0 else 1
-                    feature_weight = sign * min(abs(feature_weight), 400000.0)
+
+        # Get max edge weight for normalize edge weights
         max_edge_weight = max(abs(feature_weight), max_edge_weight)
         feature.append(feature_weight)
 
+    # Normalize edge weights
     feature = [normalize_feature(feature_weight, max_edge_weight) for feature_weight in feature]
+
+    # Save generated features for category("test" or "train")
     save_dict = pd.DataFrame({"edge_feature": feature})
     save_path = os.path.join(FLAGS.save_word_edge_features_path+"edge_feature_"+category+".csv")
     save_dict.to_csv(save_path)
-    print(max_edge_weight)
+
+    print("Maximum feature weight from generate feature", max_edge_weight)
 
 
 def preprocess_test_data(set_of_words):
